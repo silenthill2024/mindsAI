cda2e5895fe82d85b4e1e1b7d0ab10e4f7096c32
Jrcs2026 <carosalasjoseramon@gmail.com>
2026-08-04 20:07:37 -0600

    Home 4.5: ajuste dela interfaz, funcion de foto de perfil

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,45 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "com.example.mindsai"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.play.services.wearable)
+    implementation(libs.okhttp)
+    implementation(libs.kotlinx.coroutines.android)
 }
